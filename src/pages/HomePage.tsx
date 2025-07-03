786960cc20ec4e5027e512a9b1b6d8a1b174e68b
Ravshan047 <ravshanraxmatullayev34@gmail.com>
2025-07-03 15:20:03 +0500

    vobfront

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -66,13 +66,7 @@ const HomePage = () => {
             </div>
           </div>
         </div>
-
-        {/* Wave decoration */}
-        <div className="absolute bottom-0 left-0 right-0">
-          <svg xmlns="" viewBox="0 0 1440 100" className="w-full">
-            <path fill="#ffffff" fillOpacity="" d="M0,64L80,69.3C160,75,320,85,480,80C640,75,800,53,960,48C1120,43,1280,53,1360,58.7L1440,64L1440,100L1360,100C1280,100,1120,100,960,100C800,100,640,100,480,100C320,100,160,100,80,100L0,100Z"></path>
-          </svg>
-        </div>
+        {/* Wave decoration OLIB TASHLANDI */}
       </section>
 
       {/* Departments Section */}
